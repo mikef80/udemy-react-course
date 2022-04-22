2ffa42b62bf44ffa86ee57872c9cec09134c0da8
Mike <mike@mike-francis.org>
2022-04-22 22:23:45 +0100

    finished section 41

--- Assignments/react-complete-guide/src/App.js
+++ Assignments/react-complete-guide/src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses array={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- Assignments/react-complete-guide/src/components/Expenses.js
+++ Assignments/react-complete-guide/src/components/Expenses.js
@@ -1,42 +1,31 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  let outputArr = [];
-  props.array.forEach((item) => {
-    let output = (
-      <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-    );
-    outputArr.push(output);
-  });
-
-  let code = outputArr.join("");
-  console.log(code);
-
   return (
-    <div className="expenses">
-      {/* <ExpenseItem
-        title={props.array[0].title}
-        amount={props.array[0].amount}
-        date={props.array[0].date}
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={props.array[1].title}
-        amount={props.array[1].amount}
-        date={props.array[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={props.array[2].title}
-        amount={props.array[2].amount}
-        date={props.array[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
       <ExpenseItem
-        title={props.array[3].title}
-        amount={props.array[3].amount}
-        date={props.array[3].date}
-      /> */}
-      {code}
-    </div>
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
   );
 };
 
