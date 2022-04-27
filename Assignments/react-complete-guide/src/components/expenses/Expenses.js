0ea5ac69c36fc00315c4f76847bce80d5712a5ee
Mike <mike@mike-francis.org>
2022-04-27 09:04:37 +0100

    completed lecture 64

--- Assignments/react-complete-guide/src/components/expenses/Expenses.js
+++ Assignments/react-complete-guide/src/components/expenses/Expenses.js
@@ -5,18 +5,33 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filterYearValue, setFilterYearValue] = useState('2022');
+  const [filterYearValue, setFilterYearValue] = useState("2022");
 
   const filterChangeHandler = (value) => {
     // console.log(`filter changed to ${value}`);
     setFilterYearValue(value);
-  }
+  };
+
+  // console.log(props.items);
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filterYearValue} onYearChange={filterChangeHandler} />
+      <ExpensesFilter
+        selected={filterYearValue}
+        onYearChange={filterChangeHandler}
+      />
       <Card>
-        <ExpenseItem
+        {props.items.map((item) => {
+          return (
+            <ExpenseItem
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          );
+        })}
+
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -40,7 +55,7 @@ const Expenses = (props) => {
           title={props.items[4].title}
           amount={props.items[4].amount}
           date={props.items[4].date}
-        />
+        /> */}
       </Card>
     </Card>
   );
