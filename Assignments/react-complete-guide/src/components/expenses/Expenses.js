12113abecd12440d7a9cf48f306335177421c8f8
Mike <mike@mike-francis.org>
2022-04-29 08:54:06 +0100

    completed step 68

--- Assignments/react-complete-guide/src/components/expenses/Expenses.js
+++ Assignments/react-complete-guide/src/components/expenses/Expenses.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -16,26 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear() === Number(filterYearValue);
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filterYearValue}
         onYearChange={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
