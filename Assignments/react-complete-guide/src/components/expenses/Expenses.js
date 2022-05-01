ec60a50be5f0c0548bc4d093ec30ffe5a36fc7ca
Mike <mike@mike-francis.org>
2022-05-01 22:15:07 +0100

    added second assignment

--- Assignments/react-complete-guide/src/components/expenses/Expenses.js
+++ Assignments/react-complete-guide/src/components/expenses/Expenses.js
@@ -3,28 +3,31 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filterYearValue, setFilterYearValue] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (value) => {
-    // console.log(`filter changed to ${value}`);
-    setFilterYearValue(value);
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
 
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear() === Number(filterYearValue);
+    return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filterYearValue}
-        onYearChange={filterChangeHandler}
-      />
-      <ExpensesList items={filteredExpenses} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
 };
 
