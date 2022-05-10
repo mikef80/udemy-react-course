bc354143705d30c1eadba4c5133cdd2e3f4cc498
Mike <mike@mike-francis.org>
2022-05-10 08:59:58 +0100

    updating layout

--- Assignments/section-8-complete-project/src/App.js
+++ Assignments/section-8-complete-project/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import InputForm from "./components/InputForm";
+import ErrorModal from "./components/ErrorModal";
 
 import "./App.css";
 
 const App = () => {
   return (
     <div id="main">
-      <InputForm />
+      <ErrorModal />
+      {/* <InputForm /> */}
     </div>
   );
 };
---
+++ Assignments/section-8-complete-project/src/components/ErrorModal.js
@@ -0,0 +1,8 @@
+import "./ErrorModal.module.css";
+
+const ErrorModal = () => {
+  console.log("error");
+  return <div className="ErrorModal">hello</div>;
+};
+
+export default ErrorModal;
