80130983c64f9fff7214e6d7e60272a1cbec7694
Mike <mike@mike-francis.org>
2022-05-13 22:08:47 +0100

    working in outputform.js. onClick not working, unsure why, needs more looking into. trying to delete when clicked.

--- Assignments/section-8-complete-project/src/components/OutputForm.js
+++ Assignments/section-8-complete-project/src/components/OutputForm.js
@@ -5,10 +5,20 @@ import styles from "./OutputForm.module.css";
 const OutputForm = (props) => {
   // console.log(props.items);
 
+  const deleteItem = () => {
+    alert("delete thsi item");
+  };
+
   return (
     <div className={styles.OutputForm}>
       {props.items.map((item) => (
-        <UserItem key={item.id} id={item.id} name={item.name} age={item.age} />
+        <UserItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          age={item.age}
+          onClick={() => alert("click")}
+        />
       ))}
     </div>
   );
--- Assignments/section-8-complete-project/src/components/UserItem.js
+++ Assignments/section-8-complete-project/src/components/UserItem.js
@@ -2,7 +2,7 @@ import styles from "./UserItem.module.css";
 
 const UserItem = (props) => {
   return (
-    <div id={styles.UserItem}>
+    <div className={styles.UserItem}>
       <p>ID: {props.id}</p>
       <p>Name: {props.name}</p>
       <p>Age: {props.age}</p>
