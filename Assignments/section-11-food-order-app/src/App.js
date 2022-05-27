ffd577f46d36f13244efc074b27217357331dce8
Mike <mike@mike-francis.org>
2022-05-27 17:28:31 +0100

    finished 135. Need to come up with Card component to wrap UL element and also every list item should become seprate component with mroe info.

--- Assignments/section-11-food-order-app/src/App.js
+++ Assignments/section-11-food-order-app/src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
 
 import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
