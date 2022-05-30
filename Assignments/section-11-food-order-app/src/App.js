16ffe3a3b2253da553a05be629f213146911bdd5
Mike <mike@mike-francis.org>
2022-05-30 08:53:43 +0100

    finished 141

--- Assignments/section-11-food-order-app/src/App.js
+++ Assignments/section-11-food-order-app/src/App.js
@@ -1,14 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./components/cart/Cart";
 
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
