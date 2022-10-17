ef272d405bd0d72a50da71c047a891761eee9c0d
Mike <mike@mike-francis.org>
2022-10-17 22:25:23 +0100

    continuing section 12

--- Assignments/section-12/src/App.js
+++ Assignments/section-12/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
+  console.log('App running');
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
