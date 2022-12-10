877565f74fd53d6b51cdb4b6dec0f9804ea117a2
Mike <mike@mike-francis.org>
2022-12-10 21:50:10 +0000

    sync

--- Assignments/section-18/Project/src/store/index.js
+++ Assignments/section-18/Project/src/store/index.js
@@ -3,8 +3,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import authReducer from './authSlice';
 
-
-
 const store = configureStore({
   reducer: {
     counter: counterReducer,
