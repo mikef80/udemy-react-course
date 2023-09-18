571ace9f586bf2c3d12bf331c830a52af9f1d1c4
Mike <mike@mike-francis.org>
2023-09-18 22:24:25 +0100

    completed 332

--- Assignments/section-21b/frontend/src/App.js
+++ Assignments/section-21b/frontend/src/App.js
@@ -4,7 +4,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEventPage";
+import NewEventPage, { action as newEventAction } from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -63,7 +63,7 @@ const router = createBrowserRouter([
               },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
