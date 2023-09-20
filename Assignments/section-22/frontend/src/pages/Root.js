500dda390085b0a3bdeb3fa86898a6b96ac550f9
Mike <mike@mike-francis.org>
2023-09-20 16:32:13 +0100

    completed section 22

---
+++ Assignments/section-22/frontend/src/pages/Root.js
@@ -0,0 +1,39 @@
+import { Outlet, useSubmit } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { getAuthToken, getTokenDuration } from "../util/auth";
+import { useEffect } from "react";
+
+function RootLayout() {
+  const token = getAuthToken();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
