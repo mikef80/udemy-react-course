876f247a3e58546b42d7b01fc592804f14bf7103
Mike <mike@mike-francis.org>
2023-09-21 13:38:32 +0100

    finally got useQuery qorking.  Need to pass queryFn via ananoymous fn as I needed to pass parameters

--- Assignments/section-24/src/components/Events/EventDetails.jsx
+++ Assignments/section-24/src/components/Events/EventDetails.jsx
@@ -1,8 +1,63 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
+import { fetchEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  console.log(id);
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["oooh"],
+    queryFn: () => fetchEvent({ id: id }),
+  });
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+  }
+
+  if (data) {
+    console.log(data);
+    const { title, image, description, date, time, location } = data;
+    content = (
+      <article id="event-details">
+        <header>
+          <h1>{title}</h1>
+          <nav>
+            <button>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${image}`} alt="" />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {date} @ {time}
+              </time>
+            </div>
+            <p id="event-details-description">{description}</p>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,7 +66,8 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
+      {content}
+      {/* <article id="event-details">
         <header>
           <h1>EVENT TITLE</h1>
           <nav>
@@ -29,7 +85,7 @@ export default function EventDetails() {
             <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
         </div>
-      </article>
+      </article> */}
     </>
   );
 }
